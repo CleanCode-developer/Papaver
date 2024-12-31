db5beebe08060c07e9dd14bd1b776375d4cf4456
Clean Code <amin.asincorpate@gmail.com>
2024-12-30 16:55:34 -0800

    Update exportGame.js

--- js/exportGame.js
+++ js/exportGame.js
@@ -1,1 +1,21 @@
+import SceneManager from './sceneManager.js';
 
+class ExportGame {
+  constructor(sceneManager) {
+    this.sceneManager = sceneManager;
+    document.getElementById('exportGame').addEventListener('click', () => this.export());
+  }
+
+  export() {
+    const gameState = JSON.stringify(this.sceneManager);
+    const blob = new Blob([gameState], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'game.json';
+    a.click();
+    URL.revokeObjectURL(url);
+  }
+}
+
+export default ExportGame;

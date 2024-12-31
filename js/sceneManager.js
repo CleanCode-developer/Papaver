d50325ae4d6b553795c70d9a88532d5a3bb55a52
Clean Code <amin.asincorpate@gmail.com>
2024-12-30 16:54:35 -0800

    Update sceneManager.js

--- js/sceneManager.js
+++ js/sceneManager.js
@@ -1,1 +1,31 @@
+class SceneManager {
+  constructor() {
+    this.scenes = [];
+    this.activeScene = null;
+  }
 
+  addScene(scene) {
+    this.scenes.push(scene);
+    if (!this.activeScene) {
+      this.activeScene = scene;
+    }
+  }
+
+  setActiveScene(sceneName) {
+    this.activeScene = this.scenes.find(scene => scene.name === sceneName);
+  }
+
+  update() {
+    if (this.activeScene) {
+      this.activeScene.update();
+    }
+  }
+
+  render(renderer) {
+    if (this.activeScene) {
+      renderer.render(this.activeScene);
+    }
+  }
+}
+
+export default SceneManager;

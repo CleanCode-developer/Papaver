3b6aa7fe742dfe3c9633b1bad93933c0be56a1b0
Clean Code <amin.asincorpate@gmail.com>
2024-12-30 16:48:56 -0800

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,1 +1,32 @@
+import Renderer from './renderer.js';
+import SceneManager from './sceneManager.js';
+import InputHandler from './inputHandler.js';
+import ExportGame from './exportGame.js';
+import UIHandler from './uiHandler.js';
 
+class Engine {
+  constructor() {
+    this.renderer = new Renderer();
+    this.sceneManager = new SceneManager();
+    this.inputHandler = new InputHandler();
+    this.exportGame = new ExportGame(this.sceneManager);
+    this.uiHandler = new UIHandler(this.sceneManager);
+
+    this.init();
+  }
+
+  init() {
+    // Initialize scenes and other components
+    this.loop();
+  }
+
+  loop() {
+    requestAnimationFrame(() => this.loop());
+    this.sceneManager.update();
+    this.renderer.render();  // Render with Three.js
+  }
+}
+
+window.onload = () => {
+  const engine = new Engine();
+};
